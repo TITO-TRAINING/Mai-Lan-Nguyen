1676722d1767e6186a702e5fbf4595b3c53ff060
Lanng21 <mailanbiun@gmail.com>
2023-04-20 16:50:56 +0700

    Implement ex1/2/3 of chapter 5

---
+++ src/higherOrderFunctions/ex2.js
@@ -0,0 +1,28 @@
+/* Your Own Loop
+Write a higher-order function loop that provides something like a for loop
+statement. It takes a value, a test function, an update function, and a body
+function. Each iteration, it first runs the test function on the current loop
+value and stops if that returns false. Then it calls the body function, giving it
+the current value. Finally, it calls the update function to create a new value
+and starts from the beginning.
+When defining the function, you can use a regular loop to do the actual
+looping.
+ */
+
+function loop(value, testFunc, updateFunc, bodyFunc) {
+  for (let i = value; testFunc(i); i = updateFunc(i)) {
+    bodyFunc(i);
+  }
+}
+
+loop(
+  3,
+  (n) => n > 0,
+  (n) => n - 1,
+  console.log
+);
+
+// Output:
+// 3
+// 2
+// 1
